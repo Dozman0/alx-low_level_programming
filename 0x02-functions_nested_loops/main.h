3d7430c088e48c104f6fdafad07fad775859597c
Dozman0 <dozman09@yahoo.com>
2022-09-13 15:25:30 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,7 +10,7 @@ void print_alphabet_x10(void);
 
 int _islower(int c);
 
-int _isalpha(int n);
+int _isalpha(int c);
 
 int print_sign(int n);
 
@@ -22,20 +22,10 @@ void jack_bauer(void);
 
 void times_table(void);
 
-void pr_digit(int charcode, int fmt)
-
 int add(int a, int b);
-
-void pr_with_fmt(int charcode, int fmt);
 
 void print_to_98(int n);
 
-void pr_3_digit(int charcode, int fmt)
-
 void print_times_table(int n);
-
-void print_spaces(int n);
-
-void print_zero(void);
 
 #endif
