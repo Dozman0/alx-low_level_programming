c5316cbb8ea2aadadec75c6da7f66007ceda6139
Dozman0 <dozman09@yahoo.com>
2022-09-13 14:41:45 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,6 @@
-#define MAIN_H
+#ifndef MAIN_H_
+
+#define MAIN_H_
 
 int _putchar(char);
 
@@ -20,8 +22,20 @@ void jack_bauer(void);
 
 void times_table(void);
 
+void pr_digit(int charcode, int fmt)
+
 int add(int a, int b);
+
+void pr_with_fmt(int charcode, int fmt);
 
 void print_to_98(int n);
 
+void pr_3_digit(int charcode, int fmt)
+
 void print_times_table(int n);
+
+void print_spaces(int n);
+
+void print_zero(void);
+
+#endif
