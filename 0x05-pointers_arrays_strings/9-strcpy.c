06012bdb89570fe9ca06b29612451505f25e91bf
Dozman0 <dozman09@yahoo.com>
2022-09-19 15:24:13 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,6 +12,7 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	i = 0;
 
 	while (src[i] != '\0')
@@ -20,6 +21,5 @@ char *_strcpy(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
-	
 	return (dest);
 }
