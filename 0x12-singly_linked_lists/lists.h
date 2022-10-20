a7609206edd6af1f096162c92162fa33b9929994
Dozman0 <dozman09@yahoo.com>
2022-10-20 14:38:03 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,13 +13,7 @@
  *
  * Description: singly linked list node structure
  */
-
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+;
 typedef struct list_s
 {
     char *str;
